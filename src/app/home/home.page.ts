d6e31b203290782de9633540369ddd1249e8fa38
mstasiak98 <mikistasiak98@gmail.com>
2023-09-27 20:27:08 +0200

    handle gif loading and playing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RedditService} from "../shared/data-access/reddit/reddit.service";
+import {BehaviorSubject, combineLatest, map} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,42 @@ import {RedditService} from "../shared/data-access/reddit/reddit.service";
 })
 export class HomePage {
 
-  gifs$ = this.redditService.getGifs();
+  currentlyLoadingGifs$ = new BehaviorSubject<string[]>([]);
+  loadedGifs$ = new BehaviorSubject<string[]>([]);
+
+  gifs$ = combineLatest([
+    this.redditService.getGifs(),
+    this.currentlyLoadingGifs$,
+    this.loadedGifs$
+  ]).pipe(
+    map(([gifs, currentlyLoadingGifs, loadedGifs]) =>
+      gifs.map((gif) => ({
+        ...gif,
+        loading: currentlyLoadingGifs.includes(gif.permalink),
+        dataLoaded: loadedGifs.includes(gif.permalink)
+      }))
+    )
+  );
+
   constructor(private redditService: RedditService) {}
 
+  setLoading(permalink: string) {
+    this.currentlyLoadingGifs$.next([
+      ...this.currentlyLoadingGifs$.value,
+      permalink
+    ]);
+  }
+
+  setLoadingComplete(permalinkToComplete: string) {
+    this.loadedGifs$.next([
+      ...this.loadedGifs$.value,
+      permalinkToComplete
+    ]);
+
+    this.currentlyLoadingGifs$.next([
+      ...this.currentlyLoadingGifs$.value.filter(
+        (permalink) => !this.loadedGifs$.value.includes(permalink)
+      )
+    ]);
+  }
 }
--- src/app/home/ui/gif-list/gif-list.component.ts
+++ src/app/home/ui/gif-list/gif-list.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input, NgModule, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, NgModule, OnInit, Output} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {IonicModule} from "@ionic/angular";
@@ -14,9 +14,38 @@ export class GifListComponent  implements OnInit {
 
   @Input() gifs!: Gif[];
 
+  @Output() gifLoadStart = new EventEmitter<string>();
+  @Output() gifLoadComplete = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {}
+
+  playVideo(event: Event, gif: Gif) {
+    const video = event.target as HTMLVideoElement;
+
+    if(video.readyState === 4) {
+      if(video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    }else {
+      if(video.getAttribute('data-event-loadeddata') !== 'true') {
+        this.gifLoadStart.next(gif.permalink);
+        video.load();
+
+        const handleVideoLoaded = async () => {
+          this.gifLoadComplete.next(gif.permalink);
+          await video.play();
+          video.removeEventListener('loadeddata', handleVideoLoaded);
+        };
+
+        video.addEventListener('loadeddata', handleVideoLoaded);
+        video.setAttribute('data-event-loadeddata', 'true');
+      }
+    }
+  }
 
   trackByFn(index: number, gif: Gif) {
     return gif.permalink;
